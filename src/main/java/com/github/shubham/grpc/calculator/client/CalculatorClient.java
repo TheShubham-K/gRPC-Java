bd323f0e6754585740d823e643611ee6e678b1c2
subhamk <subhamk@juniper.net>
2023-06-11 23:14:39 +0530

    completed clientStreaming api

--- src/main/java/com/github/shubham/grpc/calculator/client/CalculatorClient.java
+++ src/main/java/com/github/shubham/grpc/calculator/client/CalculatorClient.java
@@ -1,25 +1,46 @@
 package com.github.shubham.grpc.calculator.client;
 
-import com.proto.calculator.CalculatorServiceGrpc;
-import com.proto.calculator.PrimeNumberDecompositionRequest;
-import com.proto.calculator.SumRequest;
-import com.proto.calculator.SumResponse;
+import com.proto.calculator.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class CalculatorClient {
+
     public static void main(String[] args) {
+        CalculatorClient main = new CalculatorClient();
+        main.run();
+    }
+
+    private void run() {
         ManagedChannel channel = ManagedChannelBuilder
                 .forAddress("localhost", 50052)
                 .usePlaintext()
                 .build();
 
+        /**
+         * APIs call
+         */
+        // doUnaryCall(channel);
+        // doServerStreamingCall(channel);
+        doClientStreamingCall(channel);
+
+        System.out.println("Shutting down the channel");
+        channel.shutdown();
+    }
+
+    private void doUnaryCall(ManagedChannel channel) {
+
+        // created a greet service client (blocking - synchronous)
+
         CalculatorServiceGrpc.CalculatorServiceBlockingStub stub
                 = CalculatorServiceGrpc.newBlockingStub(channel);
 
         // Unary API Server
-        /*
-         SumRequest request = SumRequest.newBuilder()
+        SumRequest request = SumRequest.newBuilder()
                 .setFirstNumber(10)
                 .setSecondNumber(25)
                 .build();
@@ -27,17 +48,60 @@ public class CalculatorClient {
         SumResponse response = stub.sum(request);
         System.out.println(request.getFirstNumber() + " + "
                 + request.getSecondNumber() + " = " + response.getSumResult());
+    }
 
-        */
+    private void doServerStreamingCall(ManagedChannel channel) {
+
+        CalculatorServiceGrpc.CalculatorServiceBlockingStub stub
+                = CalculatorServiceGrpc.newBlockingStub(channel);
 
         long number = 5678999089032923090L;
         stub.primeNumberDecomposition(
-                PrimeNumberDecompositionRequest.newBuilder()
-                .setNumber(number)
-                .build())
-                .forEachRemaining(primeNumberDecompositionResponse -> {
-                    System.out.println(primeNumberDecompositionResponse.getPrimeFactor());
-                });
-        channel.shutdown();
+                        PrimeNumberDecompositionRequest.newBuilder()
+                                .setNumber(number)
+                                .build())
+                .forEachRemaining(primeNumberDecompositionResponse ->
+                        System.out.println(primeNumberDecompositionResponse.getPrimeFactor())
+                );
+    }
+
+    private void doClientStreamingCall(ManagedChannel channel) {
+
+        CalculatorServiceGrpc.CalculatorServiceStub asyncStub = CalculatorServiceGrpc.newStub(channel);
+        CountDownLatch latch = new CountDownLatch(1);
+        StreamObserver<ComputeAverageRequest> requestStreamObserver = asyncStub.computeAverage(new StreamObserver<ComputeAverageResponse>() {
+            @Override
+            public void onNext(ComputeAverageResponse value) {
+
+                System.out.println("Received a response from the server");
+                System.out.println("Average from the server: "+value.getAverage());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("Server has completed sending us data");
+                latch.countDown();
+            }
+        });
+
+        // we send 10000000 messages to our server (client streaming)
+        for(int i = 0; i < 100000; i++) {
+            requestStreamObserver.onNext(ComputeAverageRequest.newBuilder()
+                    .setNumber(i)
+                    .build());
+        }
+
+        // we expect the average to be sum_of(100000) / 100000 = 7049.82704
+        requestStreamObserver.onCompleted();
+        try {
+            latch.await(3, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace(System.out);
+        }
     }
 }
--- src/main/java/com/github/shubham/grpc/calculator/server/CalculatorServiceImpl.java
+++ src/main/java/com/github/shubham/grpc/calculator/server/CalculatorServiceImpl.java
@@ -38,4 +38,38 @@ public class CalculatorServiceImpl extends CalculatorServiceGrpc.CalculatorServi
         }
         responseObserver.onCompleted();
     }
+
+    @Override
+    public StreamObserver<ComputeAverageRequest> computeAverage(StreamObserver<ComputeAverageResponse> responseObserver) {
+
+        return new StreamObserver<ComputeAverageRequest>() {
+
+            // running sum and count
+            int sum = 0;
+            int count = 0;
+            @Override
+            public void onNext(ComputeAverageRequest value) {
+                // increment the sum
+                sum += value.getNumber();
+                // increment the count
+                count += 1;
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                System.out.println("Error in compute: " + t.toString());
+            }
+
+            @Override
+            public void onCompleted() {
+
+                double average = (double) sum/count;
+                responseObserver.onNext(ComputeAverageResponse.newBuilder()
+                        .setAverage(average)
+                        .build()
+                );
+                responseObserver.onCompleted();
+            }
+        };
+    }
 }
